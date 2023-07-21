ae95841bd049b61d1d9322d1c2879deec8758cba
Pawan Yadav <90885858+PawanYadav007s@users.noreply.github.com>
2023-07-21 13:48:56 +0530

    Add files via upload

---
+++ InversionCount.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class InversionCount{
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int arr[]=new int [n];
+for(int i=0;i<n;i++){
+arr[i]=sc.nextInt();
+}
+int count=0;
+for(int i=0;i<n;i++){
+for(int j=i+1;j<n;j++){
+if(arr[i]>arr[j])
+count++;
+}
+}
+System.out.println(count);
+}
+
+}
